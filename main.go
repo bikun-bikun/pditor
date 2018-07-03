18a283b3f285c34344d910a664ef5f55d92fdee2
bikun-bikun <big.stone.net@gmail.com>
2018-07-04 01:39:26 +0900

    cli 機能調査 アプリ名、version情報の追加

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/urfave/cli"
@@ -8,5 +9,14 @@ import (
 
 func main() {
 	app := cli.NewApp()
+	app.Name = "pditor"
+	app.Usage = "make an explosive entrance"
+	app.Version = "0.0.1"
+
+	app.Action = func(c *cli.Context) error {
+		fmt.Println("pditor! I say!")
+		return nil
+	}
+
 	app.Run(os.Args)
 }
