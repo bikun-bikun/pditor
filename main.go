ac163b0b52724b6ebd74331b6c5142e001955a79
bikun-bikun <big.stone.net@gmail.com>
2018-07-07 15:59:34 +0900

    メインコマンドとフラグの大枠を追加
    メインコマンド：rotation PDFファイルを回転させる。
    メインコマンドの設定用フラグ：
    * pages: ページの指定、数値指定ではなく全ページ、奇,偶数ページごとに指定するような感じでどうだろうか？
    * left-angle: 左回転の角度指定基本90度ごとでエラーハンドルしたい。
    * right-angle: 右回転の角度指定基本90度ごとでエラーハンドルしたい。
    
    todo:
    * 他にも必要なフラグあるかどうかを考えておく。
    * left,right-angleについては両方指定された場合どうするかを考える必要がある。

--- main.go
+++ main.go
@@ -13,6 +13,35 @@ func main() {
 	app.Usage = "make an explosive entrance"
 	app.Version = "0.0.1"
 
+	app.Commands = []cli.Command{
+		{
+			Name:    "rotation",
+			Usage:   "PDF File rotation",
+			Aliases: []string{"r"},
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:  "pages, p",
+					Usage: "default all page rotate, Odd page option=o Even page option=e",
+					Value: "all",
+				},
+				cli.Int64Flag{
+					Name:  "left-angle, la",
+					Usage: "This flag is left rotation angle setting values{0, 90, 180, 270}",
+					Value: 0,
+				},
+				cli.Int64Flag{
+					Name:  "right-angle, ra",
+					Usage: "This flag is right rotation angle setting values{0, 90, 180, 270}",
+					Value: 0,
+				},
+			},
+			Action: func(c *cli.Context) error {
+				fmt.Println("Rotate PDF!!!")
+				return nil
+			},
+		},
+	}
+
 	app.Action = func(c *cli.Context) error {
 		fmt.Println("pditor! I say!")
 		return nil
